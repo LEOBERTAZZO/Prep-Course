2e5ea31d37c1755048cc0024a61ceeec522d22ac
leobertazzo <ibanez5702003@hotmail.com>
2022-06-22 21:08:27 -0300

    third commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = '123';
+const nuevoNum = '12';
 
 // Crea una variable booleana:
 const nuevoBool = 2==2;
