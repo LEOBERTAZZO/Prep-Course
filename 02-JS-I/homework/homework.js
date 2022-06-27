223c6b36f04c565274f22e6335ace7ebf86fcc2f
leobertazzo <ibanez5702003@hotmail.com>
2022-06-27 18:16:57 -0300

    223 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -225,7 +225,7 @@ function deEuroAdolar(euro){
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
   
-}
+} return (euro * 1.20) 
 
 
 function esVocal(letra){
