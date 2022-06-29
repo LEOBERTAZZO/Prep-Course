3eec5d183c47ef054042b17a0768eda8e085c91d
leobertazzo <ibanez5702003@hotmail.com>
2022-06-29 17:34:28 -0300

    176 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -178,14 +178,14 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
- if (numero > 0){
+  if (numero === 0) {
+    return false; 
+  }
+ if  (numero > 0){
   return "Es positivo";
 }   else (numero < 0)  
   {return "Es negativo"; }
-  if (numero === 0){ 
-    return "false";
-  }
+  
 
 
 } 
