a818a271f12d0e25af3ba334f005ddcc99c00900
leobertazzo <ibanez5702003@hotmail.com>
2022-06-29 12:49:43 -0300

    186 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -187,6 +187,7 @@ function agregarSimboloExclamacion() {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str + "!";
 }
 
 function combinarNombres() {
