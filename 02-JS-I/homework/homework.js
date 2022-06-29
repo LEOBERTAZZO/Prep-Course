9365485aa4bb51b5fae7b5296b7d1915e65040da
leobertazzo <ibanez5702003@hotmail.com>
2022-06-29 19:32:52 -0300

    158 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -159,14 +159,14 @@ function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
   
- return Math.round (num) 
+ return Math.round (num);
 
  } 
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  return Math.ceil (num)
+  return Math.ceil(num)
   
 }
 
