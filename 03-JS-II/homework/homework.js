085fad8c40728bb4dc48d9bd442e5b54d5eebfa2
leobertazzo <ibanez5702003@hotmail.com>
2022-06-30 21:05:31 -0300

    14 commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -15,7 +15,9 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
-}
+  if (edad >18); {return "Alloed"};
+  else (edad <18); {return "Not alloed"};
+} 
   
 function conection(status) {
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
