6ef6898fb03500dbe42a55c50ba7948760bb2382
leobertazzo <ibanez5702003@hotmail.com>
2022-07-04 19:40:10 -0300

    40 commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -44,10 +44,10 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-  if (idioma === "aleman") {return "Guten tag!"}
+  if (idioma === "aleman") {return "Guten Tag!"}
   else if (idioma === "mandarin") {return "Ni Hao!"}
   else if (idioma === "ingles") {return "Hello!"}
-  else {return "undefined"};
+  else {return "Hola!"};
 }  
 
 function colors(color) {
