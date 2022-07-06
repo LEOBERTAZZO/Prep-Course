a1005ab7e19be319f656cfcb6bb8702ee4faf918
leobertazzo <ibanez5702003@hotmail.com>
2022-07-05 21:09:17 -0300

    71 commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -72,9 +72,9 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
- if (numero === 10 || 5) {return "true"}
+ if (numero === 10 || 5) {return true}
  
-   {return "false"}; 
+   {return false}; 
 }  
 
 function estaEnRango(numero) {
