3fdf3ccc278add538d9c4c87995ee135a81e9b9e
leobertazzo <ibanez5702003@hotmail.com>
2022-07-08 17:33:53 -0300

    80 commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -81,8 +81,8 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código: 
-  if (numero <50) {return "true"}
-  else if (numero >20 ) {return "true"}
+  if (numero <50 && numero >20 ) {return "true"}
+  
   else {return "false"};
 } 
 
