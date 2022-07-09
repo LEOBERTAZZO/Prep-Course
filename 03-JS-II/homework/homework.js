c07fb32672733c685b53de232dccb14109b5b88a
leobertazzo <ibanez5702003@hotmail.com>
2022-07-09 16:36:27 -0300

    77 commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -78,7 +78,7 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
- if (numero === 10 || 5) {return true;}
+ if (numero === 10 ||numero === 5) {return true;}
  
    return false;
 }  
