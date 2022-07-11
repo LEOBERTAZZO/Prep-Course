0e68bba5300ca9091faa5cd5f49d1a6983be955b
leobertazzo <ibanez5702003@hotmail.com>
2022-07-10 22:28:17 -0300

    35 commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -36,7 +36,10 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-  return array.push(elemento);
+   array.push(elemento);
+   return array;
+   
+
 } 
 
 
