0366d96ec71c9c3883b13163bc3a8285e60f1b2c
leobertazzo <ibanez5702003@hotmail.com>
2022-07-11 21:00:27 -0300

    74 commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -75,7 +75,11 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-}
+  for (let i=0;i<array.length;i++) 
+  {if (array[i]===elemento) return true;}
+  return false 
+
+} 
 
 
 function agregarNumeros(numeros) {
