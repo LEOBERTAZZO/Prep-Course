8f847249dbbe373d46536283d9f4433973c2ca64
leobertazzo <ibanez5702003@hotmail.com>
2022-07-18 17:22:20 -0300

    138 commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -138,8 +138,14 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+  var elementos = 0; 
+  for (var i =0; i< arreglo.length; i++);{
+  if (arreglo[i] >18) elementos = elementos +1; }
 
-}
+  return elementos; 
+
+
+} 
 
 
 function diaDeLaSemana(numeroDeDia) {
