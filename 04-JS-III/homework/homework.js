bdf07b80d019c4203a7d0e7f2786289e54f846f9
leobertazzo <ibanez5702003@hotmail.com>
2022-07-18 22:07:46 -0300

    174 commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -175,8 +175,11 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
-}
+  var string = n.toString()
+   if (string(n)[0] ==="9") {return true; }
+   else return false;
+
+} 
 
 
 function todosIguales(arreglo) {
