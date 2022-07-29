fc7d457a44f9532d0085ed8f448051719b2c0ad5
leobertazzo <ibanez5702003@hotmail.com>
2022-07-28 21:15:09 -0300

    21 commit

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -23,7 +23,12 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-}
+  var miAuto = {
+    objeto: null,
+    property: fordFiesta,
+  };
+  return miAuto; 
+} 
 
 function invocarMetodo (objeto, metodo) {
   // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
